1fe4842119fd1232f8059c35da6eee9fc45a7461
JL710 <jan.lyncker@freenet.de>
2021-07-19 01:07:34 +0200

    0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='SimpleTools',
-	version='0.0.1',
+	version='0.0.2',
 	description='Simple Tools for Python',
 	py_modules=["SimpleTools"],
 	package_dir={'': 'src'}
--- src/SimpleTools.py
+++ src/SimpleTools.py
@@ -1,3 +1,50 @@
+#Simpletools 0.0.2
+
 def ping():
 	print("ping")
 	return True
+
+# works with data in one line from the file
+class OneLine:
+	def __init__(self, file="", separation=""):
+		self.file = file
+		self.separation = separation
+		tempfile = open(self.file, "r")
+		self.data = tempfile.readline().split(self.separation)
+		tempfile.close()
+		#print(self.data)
+
+		#adds the element to the file and data
+	def addElement(self, element):
+		self.data.append(element)
+		tempcontent = ""
+		for content in self.data:
+			tempcontent += (content + self.separation)
+		tempfile = open(self.file, "w")
+		tempfile.write(tempcontent[:-len(self.separation)])
+		tempfile.close()
+		#print(self.data)
+
+		#removes the element to the file and data
+	def removeElement(self, element):
+		self.data.remove(element)
+		tempcontent = ""
+		for content in self.data:
+			tempcontent += (content + self.separation)
+		tempfile = open(self.file, "w")
+		tempfile.write(tempcontent[:-len(self.separation)])
+		tempfile.close()
+		#print(self.data)
+
+		#gives all information about the data
+	def info(self):
+		#print(
+		#	f"file: {self.file},",
+		#	f"seperation: {self.separation},",
+		#	f"data: {self.data}"
+		#	)
+		return (
+			f"file: {self.file}",
+			f"seperation: {self.separation}",
+			f"data: {self.data}"
+		)
